86dd9e137f00f4af25bf74c3e0462bed0cd46a87
Ryoko Oka <ryooo.kooo.22@gmail.com>
2021-03-11 20:36:13 -0500

    adding edit page

--- admin/index.php
+++ admin/index.php
@@ -1,23 +1,31 @@
 <?php
 require_once '../load.php';
 confirm_logged_in();//only login in user can see the index.php page
-
-
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Welcome Page</title>
 </head>
+
 <body>
     <h2>Hello <?php echo $_SESSION['user_name'];?>! Welcome To Our Page. </h2>
-    <h4>IP address:  <?php echo $_SESSION['user_ip'];?></h4>
-    <h4>Last Login Time: <?php echo $_SESSION['user_lastlogintime']; ?></h4>
-    <h4>Number of Successful Login: <?php echo $_SESSION['success_login_number'];?></h4>
+
+    <h3>You are in level: <?php echo getCurrentUserLevel();?></h3>
+
+
     <br>
-    <a href="admin_createuser.php">Create User</a>
-    <a href="admin_logout.php">Sign Out</a></body>
+    
+    <?php if (isCurrentUserAdminAbove()):?>
+        <a href="admin_createuser.php">Create User</a>
+    <?php endif;?> 
+           
+    <a href="admin_edituser.php">Edit User</a>
+    <a href="admin_logout.php">Sign Out</a>
+
+    </body>
 </html>
--- admin/scripts/user.php
+++ admin/scripts/user.php
@@ -1,12 +1,35 @@
 <?php
 
+function getUserLevelMap() // the name start from verb
+{
+    return array(
+        '0'=>'Web Editor',
+        '1'=>'Web Admin',
+    );
+}
+
+function getCurrentUserLevel()
+{
+    $user_level_map = getUserLevelMap();
+
+    if (isset($_SESSION['user_level']) && array_key_exists($_SESSION['user_level'], $user_level_map)) {
+        return $user_level_map[$_SESSION['user_level']];
+    } else {
+        return "Unrecognized";
+    }
+}
+
 function createUser($user_data)
 {
+    if(empty($user_data['username'])||isUsernameExists($user_data['username'])){
+        return 'Username is invalid!';
+    }
+
     ## 1. Run the proper SQL query to insert user
     $pdo = Database::getInstance()->getConnection();
 
-    $create_user_query = 'INSERT INTO tbl_user(user_fname, user_name, user_pass, user_email)';
-    $create_user_query .= ' VALUES(:fname, :username, :password, :email)';
+    $create_user_query = 'INSERT INTO tbl_user(user_fname, user_name, user_pass, user_email, user_level)';
+    $create_user_query .= ' VALUES(:fname, :username, :password, :email, :user_level)';
  
     $create_user_set = $pdo->prepare($create_user_query);
     $create_user_result = $create_user_set->execute(
@@ -15,6 +38,7 @@ function createUser($user_data)
             ':username'=> $user_data['username'],
             ':password'=> $user_data['password'],
             ':email'=> $user_data['email'],
+            ':user_level'=> $user_data['user_level'],
         )
     );
 
@@ -53,15 +77,84 @@ function createUser($user_data)
     }
 }
 
-function randomPassword() {
-    $alphabet = "abcdefghijklmnopqrstuwxyzABCDEFGHIJKLMNOPQRSTUWXYZ0123456789";
-    $password = array(); //remember to declare $pass as an array
-    $alphaLength = strlen($alphabet) - 1; //put the length -1 in cache
-    for ($i = 0; $i < 8; $i++) {
-        $n = rand(0, $alphaLength);
-        $password[] = $alphabet[$n];
+// function randomPassword() {
+//     $alphabet = "abcdefghijklmnopqrstuwxyzABCDEFGHIJKLMNOPQRSTUWXYZ0123456789";
+//     $password = array(); //remember to declare $pass as an array
+//     $alphaLength = strlen($alphabet) - 1; //put the length -1 in cache
+//     for ($i = 0; $i < 8; $i++) {
+//         $n = rand(0, $alphaLength);
+//         $password[] = $alphabet[$n];
+//     }
+//     return implode($password); //turn the array into a string
+// }
+
+function getSingleUser($user_id){
+    // ## Testing line
+    // echo 'You are trying to fetch user:' .$user_id; 
+
+    $pdo = Database::getInstance()->getConnection();
+
+    $get_user_query = 'SELECT * FROM tbl_user WHERE user_id = :id';
+    $get_user_set = $pdo->prepare($get_user_query);
+    $result = $get_user_set->execute(
+        array(
+            ':id'=>$user_id
+        )
+    );
+
+    if($result && $get_user_set->rowCount()){
+        return $get_user_set;
+    } else {
+        return false;
     }
-    return implode($password); //turn the array into a string
 }
 
+function editUser($user_data){
+    if(empty($user_data['username'])||isUsernameExists($user_data['username'])){
+        return 'Username is invalid!';
+    }
+    $pdo = Database::getInstance()->getConnection();
 
+    ## Finish the SQL query in here
+    $update_user_query = 'UPDATE tbl_user SET user_fname=:fname, user_name=:username, user_pass=:password, user_email=:email, user_level=:user_level WHERE user_id = :id';
+    $update_user_set = $pdo->prepare($update_user_query);
+    $update_user_result = $update_user_set->execute(
+        array(
+            ':fname'        => $user_data['fname'],
+            ':username'     => $user_data['username'],
+            ':password'     => $user_data['password'],
+            ':email'        => $user_data['email'],
+            ':user_level'   => $user_data['user_level'],
+            ':id' => $user_data['id'],
+        )
+    );
+
+    // $update_user_set->debugDumpParams();
+    // exit;
+
+    if($update_user_result){
+        $_SESSION['user_name'] = $user_data['fname'];
+        $_SESSION['user_level'] = $user_data['user_level'];
+        redirect_to('index.php');
+    } else {
+        return 'Guess you got canned...';
+    }
+}
+
+function isCurrentUserAdminAbove(){
+    return !empty($_SESSION['user_level']);
+}
+
+function isUsernameExists($username){
+    $pdo = Database::getInstance()->getConnection();
+
+    $user_exists_query = 'SELECT COUNT(*) FROM tbl_user WHERE user_name = :username';
+    $user_exists_set = $pdo->prepare($user_exists_query);
+    $user_exists_result = $user_exists_set->execute(
+        array(
+            ':username'=>$username
+        )
+    );
+
+    return !$user_exists_result || $user_exists_set->fetchColumn()>0;
+}
